3839ce697a892b59b756e836c2026a975c5a7095
Marília Costa Rosendo Silva <marilia.costa.silva@usp.br>
2023-05-03 23:25:01 -0300

    Commiting experiment python files

---
+++ experiments/embedding_generation.py
@@ -0,0 +1,54 @@
+"""
+This .py file creates sentence embeddings for all the hate speech
+datasets
+"""
+# %% loading libraries
+import os
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from transformers import (
+    AutoTokenizer,
+    TFAutoModel,
+)
+
+# %%
+
+def sent_transformer(name_lm: str, df_dataset: pd.DataFrame) -> pd.DataFrame:
+    """
+    This function embeds instances based on sentence transformers.
+    input - param:
+    name_lm (string):
+    language model name
+    df_dataset (pandas dataframe):
+    dataframe with two columns. The first is the text column
+    and the second has the labels
+
+    output:
+    df_final_embedding (pandas dataframe):
+    dataframe with text (embedded) in the first column and the target (label)
+    in the second column
+    """
+    language_model = SentenceTransformer(name_lm)
+    sentences = list(df_dataset[NAME_TEXT_COLUMN])
+    embedding = language_model.encode(sentences)
+    embed_list = embedding.tolist()
+
+    df_embed = pd.DataFrame({NAME_TEXT_COLUMN: embed_list})
+    targets = df_dataset[NAME_TARGET_COLUMN]
+    df_final_embedding = pd.concat((df_embed, targets), axis=1)
+    df_final_embedding.columns = ["text","label"]
+
+    return df_final_embedding
+# %%
+os.chdir("./Hate_Speech")
+list_selected_files = os.listdir()
+NAME_TEXT_COLUMN = "text"
+NAME_TARGET_COLUMN = "label"
+
+for selected_file in list_selected_files:
+    df_content = pd.read_csv(selected_file,sep=";")
+    embed_df = sent_transformer(
+                    name_lm="sentence-transformers/all-MiniLM-L6-v2",
+                    df_dataset=df_content)
+    name_pickle_file = selected_file[:-4]
+    embed_df.to_pickle(f"embedded_{name_pickle_file}.pkl")
---
+++ experiments/lr_baseline.py
@@ -0,0 +1,96 @@
+"""
+This .py file has the logistic regression and XGBoost baselines
+"""
+
+# %% loading libraries
+
+import pandas as pd
+import numpy as np
+from scipy.stats import uniform
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import balanced_accuracy_score
+import warnings
+warnings.filterwarnings("ignore")
+
+# %% creating search spaces
+def get_search_space_hpo_logistic_regression() -> dict:
+    """
+    it returns the search space for the logistic regression
+
+    output:
+    dict_search_space (dict):
+    it has the search space for the classifier
+    """
+    dict_search_space = {
+            "penalty": ["l2", "none"],
+            "C": [0,0.5,1,1.5,2],
+            "random_state": [0, 5, 10, 20, 30, 42, 100],
+            "solver": ["newton-cg", "lbfgs", "sag", "saga"],
+            "multi_class": ["auto", "ovr"]  
+        }
+    return dict_search_space
+
+
+
+# %% loading training and test sets
+
+list_files = [['embedded_hs_01_bin_test_0_1.pkl',
+                'embedded_hs_01_bin_train_0_1.pkl'],
+                ['embedded_hs_02_multi_test.pkl',
+                'embedded_hs_02_multi_train.pkl'],
+                ["embedded_hs_03_bin_test_0_1.pkl",
+                 "embedded_hs_03_bin_train_0_1.pkl"],
+                ['embedded_hs_04_bin_test_0_1.pkl',
+                'embedded_hs_04_bin_train_0_1.pkl'],
+                ['embedded_hs_05_multi_test.pkl',
+                'embedded_hs_05_multi_train.pkl'],
+                ['embedded_hs_06_bin_test_0_1.pkl',
+                'embedded_hs_06_bin_train_0_1.pkl'],
+                ['embedded_hs_07_bin_test_0_1.pkl',
+                'embedded_hs_07_bin_train_0_1.pkl'],
+                ['embedded_hs_08_bin_test_0_1.pkl',
+                'embedded_hs_08_bin_train_0_1.pkl'],
+                ['embedded_hs_09_bin_test_0_1.pkl',
+                'embedded_hs_09_bin_train_0_1.pkl'],
+                ['embedded_hs_10_bin_test_0_1.pkl',
+                'embedded_hs_10_bin_train_0_1.pkl'],
+                ['embedded_hs_11_bin_test_0_1.pkl',
+                'embedded_hs_11_bin_train_0_1.pkl'],
+                ['embedded_hs_12_bin_test_0_1.pkl',
+                'embedded_hs_12_bin_train_0_1.pkl'],
+                ['embedded_hs_13_multi_test.pkl',
+                'embedded_hs_13_multi_train.pkl']
+                ]
+
+for item_pair in list_files:
+    print(f"item 0: {item_pair[0]} and item 1: {item_pair[1]}")
+    df_test = pd.read_pickle(item_pair[0])
+    df_train = pd.read_pickle(item_pair[1])
+    X_train = np.array(df_train["text"].tolist())
+    y_train = np.array(df_train["label"])
+    X_test = np.array(df_test["text"].tolist())
+    y_true = np.array(df_test["label"])
+    dict_lr = get_search_space_hpo_logistic_regression()
+    logistic = LogisticRegression(warm_start= False,
+                                n_jobs= -1,
+                                l1_ratio= None,
+                                verbose= 0,
+                                max_iter= 100,
+                                dual= False,
+                                tol= 10**-4,
+                                fit_intercept= True,
+                                intercept_scaling= 1,
+                                class_weight= None
+                                )
+    clf = RandomizedSearchCV(estimator = logistic,
+                            param_distributions = dict_lr,
+                            random_state=42,
+                            scoring="balanced_accuracy") # cv=5 by default
+    r_search = clf.fit(X_train, y_train)
+    y_pred = r_search.predict(X_test)
+
+    lr_bal_acc_score = balanced_accuracy_score(y_true, y_pred)
+    print(f"the balanced accuracy of {item_pair[0][9:-4]} is {lr_bal_acc_score}")
+    with open(f"bal_acc_{item_pair[0][9:-4]}.txt", "w") as new_file:
+        new_file.write(f"{item_pair[0][9:-4]};{lr_bal_acc_score};logistic regression\n")
