4bca97f40c67f2a7f90a9a4f869c6e5b541d8160
Marília Costa Rosendo Silva <67007661+marilia-cr-silva@users.noreply.github.com>
2023-12-16 11:55:54 -0300

    Renaming Auto-Sklearn file and adding hs_14 and hs_15

--- experiments/autosklearn.py
+++ experiments/autosklearn.py
@@ -1,12 +1,21 @@
 # %%
-import os
-import warnings
+@inproceedings{autosklearn_2015,
+author = {Feurer, Matthias and Klein, Aaron and Eggensperger, Katharina and Springenberg, Jost Tobias and Blum, Manuel and Hutter, Frank},
+title = {Efficient and Robust Automated Machine Learning},
+year = {2015},
+booktitle = {Proceedings of the 28th International Conference on Neural Information Processing Systems},
+pages = {2755–2763},
+}
 
+# %%
+import os
 import autosklearn.classification
 import numpy as np
 import pandas as pd
 from sklearn.metrics import balanced_accuracy_score
-
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+import warnings
 warnings.filterwarnings("ignore")
 # %% loading training and test sets
 
@@ -35,7 +44,11 @@ list_files = [['embedded_hs_01_bin_test_0_1.pkl',
               ['embedded_hs_12_bin_test_0_1.pkl',
                'embedded_hs_12_bin_train_0_1.pkl'],
               ['embedded_hs_13_multi_test.pkl',
-               'embedded_hs_13_multi_train.pkl']
+               'embedded_hs_13_multi_train.pkl'],
+              ["embedded_hs_14_bin_test_0_1.pkl",
+               "embedded_hs_14_bin_train_0_1.pkl"],
+              ["embedded_hs_15_multi_test.pkl",
+               "embedded_hs_15_multi_train.pkl"],
               ]
 
 # %%
@@ -54,6 +67,13 @@ for item_pair in list_files:
     automl_sklearn.fit(X_train, y_train)
     y_pred = automl_sklearn.predict(X_test)
     bal_acc_score = balanced_accuracy_score(y_true, y_pred)
+    try:
+      f1_calc_score = f1_score(y_true, y_pred, average="macro")
+    except Exception:
+      f1_calc_score = 0
+    acc_score = accuracy_score(y_true, y_pred)
     print(f"the balanced accuracy of {item_pair[0][9:-4]} is {bal_acc_score}")
     with open(f"autosklearn_bal_acc_{item_pair[0][9:-4]}.txt", "w") as new_file:
-        new_file.write(f"{item_pair[0][9:-4]};{bal_acc_score};autosklearn\n")
+        new_file.write(
+            f"{item_pair[0][9:-4]}; acc: {acc_score}; bal acc: {bal_acc_score}; f1-score: {f1_calc_score}; autosklearn\n"
+        )
